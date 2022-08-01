2a9cffeacaa1b5ea906ed478a0c5c4ddecb5fa8c
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-08-01 17:07:06 +0200

    Remove unused scripts

--- scripts/k3s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install k3s
-curl -sfL https://get.k3s.io | sh -
-
-# Copy kubeconfig file to vagrant shared directory.
-cp /etc/rancher/k3s/k3s.yaml /artifacts/
--- scripts/rhel/basic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-dnf -y update
-dnf -y install make nano jq wget golang kernel-devel origin-clients
